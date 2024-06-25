ac23c024eb1c69ee5f17adef1439b558680dcb5a
Guillem Arias Fauste <hello@gariasf.com>
2024-06-25 10:55:16 +0200

    init graphics pipeline and read shader files

--- app.hpp
+++ app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
 	class App {
@@ -12,5 +13,6 @@ namespace lve {
 		void run();
 	private:
 		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello World" };
+		LvePipeline lvePipeline{ "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv" };
 	};
 }
